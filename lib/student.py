014c5b6d3f0c5c08eb293cb9b433351905b9c182
muqtar <muqtarabdiaziz8@gmail.com>
2023-11-30 12:44:16 +0300

    comP

--- lib/student.py
+++ lib/student.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
 from user import User
-
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []  # Initialize knowledge as an empty list for each student
+
+    def learn(self, new_knowledge):
+        self.knowledge.append(new_knowledge)
+
